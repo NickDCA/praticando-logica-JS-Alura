f3b1162b9cb325ff04b98050ce9521506cf22a19
nicolasdca <nicolasdavid193@gmail.com>
2023-10-26 09:38:29 -0300

    AluGames Resolução Alura

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,24 +1,36 @@
-const jogo1 = document.getElementById('game-1');
-const jogo2 = document.getElementById('game-2');
-const jogo3 = document.getElementById('game-3');
-const jogos = [jogo1, jogo2, jogo3];
+// MINHA SOLUÇÃO
+// const jogo1 = document.getElementById('game-1');
+// const jogo2 = document.getElementById('game-2');
+// const jogo3 = document.getElementById('game-3');
+// const jogos = [jogo1, jogo2, jogo3];
 
-/*
-TODO
-- Função alterarStatus(x)
-*/
+// function alterarStatus(id) {
+//   const jogoEscolhido = jogos[id - 1];
+//   const jogoEscolhidoImg = jogoEscolhido.children[0];
+//   const jogoEscolhidoBt = jogoEscolhido.children[2];
 
-function alterarStatus(jogo) {
-  const jogoEscolhido = jogos[jogo - 1];
-  const jogoEscolhidoImg = jogoEscolhido.children[0];
-  const jogoEscolhidoBt = jogoEscolhido.children[2];
-  const jogoEscolhidoStatus = jogoEscolhidoImg.classList;
+//   if (jogoEscolhidoBt.classList.contains('dashboard__item__button--return')) {
+//     jogoEscolhidoBt.innerHTML = 'Alugar';
+//   } else {
+//     jogoEscolhidoBt.innerHTML = 'Devolver';
+//   }
+//   jogoEscolhidoBt.classList.toggle('dashboard__item__button--return');
+//   jogoEscolhidoImg.classList.toggle('dashboard__item__img--rented');
+// }
 
-  if (jogoEscolhidoBt.classList.contains('dashboard__item__button--return')) {
-    jogoEscolhidoBt.innerHTML = 'Alugar';
+// SOLUÇÃO ALURA
+function alterarStatus(id) {
+  let gameClicado = document.getElementById(`game-${id}`);
+  let imagem = gameClicado.querySelector('.dashboard__item__img');
+  let botao = gameClicado.querySelector('.dashboard__item__button');
+
+  if (imagem.classList.contains('dashboard__item__img--rented')) {
+    imagem.classList.remove('dashboard__item__img--rented');
+    botao.textContent = 'Alugar';
+    botao.classList.remove('dashboard__item__button--return');
   } else {
-    jogoEscolhidoBt.innerHTML = 'Devolver';
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.textContent = 'Devolver';
+    botao.classList.add('dashboard__item__button--return');
   }
-  jogoEscolhidoBt.classList.toggle('dashboard__item__button--return');
-  jogoEscolhidoImg.classList.toggle('dashboard__item__img--rented');
 }
